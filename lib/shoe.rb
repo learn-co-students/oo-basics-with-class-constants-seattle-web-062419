1daace1ceec065d39cf12422e328dc4c797dce11
github username <github email address>
2019-06-25 16:36:56 -0700

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,20 @@
+require 'pry'
+
+# keeps track of all brands
+# only keeps track of unique brands
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = [] 
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
 end
